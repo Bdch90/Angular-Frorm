f5350875b89d52d7a0b5ff0335ae34abe14d1705
Your Name <you@example.com>
2019-06-11 11:22:45 +0700

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { TodoComponent } from './todo/todo.component';
+import { from } from 'rxjs';
+import { RegisterFinalComponent } from './register-final/register-final.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenFormComponent,
+    TodoComponent,
+    RegisterFinalComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl, FormsModule } from '@angular/forms';
+
+interface ITodo {
+  id: number;
+  content: string;
+  complete: boolean;
+}
+let id1 = 1;
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent implements OnInit {
+  userInput = new FormControl();
+  todos: Array<ITodo> = [];
+  constructor() { }
+  ngOnInit() {
+  }
+  onChange() {
+    const { value } = this.userInput;
+    if (value) {
+      const todo: ITodo = {
+        id: id1++,
+        content: value,
+        complete: false
+      };
+      this.todos.push(todo);
+      this.userInput.setValue('');
+    }
+  }
+  toggleTodo(i) {
+    this.todos[i].complete = !this.todos[i].complete;
+  }
+}
